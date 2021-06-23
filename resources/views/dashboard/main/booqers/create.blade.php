84438d64ec378718a60da60ca34ea119f69c8f27
Shinta0212 <destiera02@gmail.com>
2021-06-23 18:47:01 +0700

    Creat

---
+++ resources/views/dashboard/main/booqers/create.blade.php
@@ -0,0 +1,124 @@
+@extends('template')
+@section('title', 'Add Booqers')
+@section('header')
+  <!-- Custom fonts for this template-->
+  <link href="{{asset('assets/dashboard/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="{{asset('assets/dashboard/css/sb-admin-2.min.css')}}" rel="stylesheet">
+@endsection
+@section('content')
+<body id="page-top">
+
+  <!-- Page Wrapper -->
+  <div id="wrapper">
+
+    <!-- Sidebar -->
+      @include('dashboard.layout.sidebar')
+    <!-- End of Sidebar -->
+
+    <!-- Content Wrapper -->
+    <div id="content-wrapper" class="d-flex flex-column">
+
+      <!-- Main Content -->
+      <div id="content">
+
+        <!-- Topbar -->
+        @include('dashboard.layout.topbar')
+        <!-- End of Topbar -->
+
+        <!-- Begin Page Content -->
+        <div class="container-fluid">
+          <div class="card shadow mb-4">
+            <div class="card-header py-3 d-flex justify-content-between">
+              <h6 class="m-0 font-weight-bold text-primary">Add Booqers</h6>
+            </div>
+            <div class="card-body">
+              @if ($errors->any())
+                <div class="alert alert-danger">
+                    @foreach ($errors->all() as $error)
+                        {{ $error }}<br>
+                    @endforeach
+                </div>
+              @endif
+              <form method="POST" action="{{route('booqer.store')}}">
+                @csrf
+              <div class="form-group">
+                <label for="email" class="form-label">Email</label>
+                <input type="text" class="form-control" id="email" name="email" value="{{ old('email') }}">
+              </div>
+              <div class="form-group">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" name="password">
+              </div>
+              <div class="form-group">
+                <label for="repass" class="form-label">Re Password</label>
+                <input type="password" class="form-control" id="repass" name="password_confirmation">
+              </div>
+              <div class="form-group">
+                <label for="name" class="form-label">Full Name</label>
+                <input type="text" class="form-control" id="name" name="name" value="{{ old('name') }}">
+              </div>
+              <div class="form-group">
+                <label for="address" class="form-label">Address</label>
+                <input type="text" class="form-control" id="address" name="address" value="{{ old('address') }}">
+              </div>
+              <div class="form-group">
+                <label for="phone" class="form-label">Phone</label>
+                <input type="text" class="form-control" id="phone" name="phone" value="{{ old('phone') }}">
+              </div>
+              <label for="city" class="form-label">City</label>
+              <div class="form-group">
+                <select name="city" id="city" class="form-control">
+                  @foreach($city as $cityData)
+                  <option value="{{$cityData->id}} {{$cityData->province_id}}">{{$cityData->city_name}}</option>
+                  @endforeach
+                </select>
+              </div>
+              <div class="form-group">
+                <input type="submit" value="buat" class="form-control btn btn-primary">
+              </div>
+              </form>
+            </div>
+          </div>
+        </div>
+        <!-- /.container-fluid -->
+
+      </div>
+      <!-- End of Main Content -->
+
+      <!-- Footer -->
+      <footer class="sticky-footer bg-white">
+        <div class="container my-auto">
+          <div class="copyright text-center my-auto">
+            <span>Copyright &copy; Your Website 2019</span>
+          </div>
+        </div>
+      </footer>
+      <!-- End of Footer -->
+
+    </div>
+    <!-- End of Content Wrapper -->
+
+  </div>
+  <!-- End of Page Wrapper -->
+
+  <!-- Scroll to Top Button-->
+  <a class="scroll-to-top rounded" href="#page-top">
+    <i class="fas fa-angle-up"></i>
+  </a>
+
+</body>
+@endsection
+@section('footer')
+  <!-- Bootstrap core JavaScript-->
+  <script src="{{asset('assets/dashboard/vendor/jquery/jquery.min.js')}}"></script>
+  <script src="{{asset('assets/dashboard/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="{{asset('assets/dashboard/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="{{asset('assets/dashboard/js/sb-admin-2.min.js')}}"></script>
+@endsection
